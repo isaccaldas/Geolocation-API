3639d4602c580e58f003b50f08c9253c444c7468
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-03-07 20:16:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ function showPosition(position) {
         Longitude: ${position.coords.longitude}
     `;
 }
+
+//testes
+//var boletim=[[8,7,9,3],[4,5,8,6]]; console.log(boletim[1][2]);
+//setTimeout("console.log('Hello');",5000); // faz o texto aparecer após 5 s. 
